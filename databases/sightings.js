6a168b50551d43b5e4436e596fc2336e9d79e247
JamesBell15 <73955873+JamesBell15@users.noreply.github.com>
2023-03-16 12:12:18 +0000

    Add sightings db

---
+++ databases/sightings.js
@@ -0,0 +1,15 @@
+let mongoose = require('mongoose');
+let mongoDB = 'mongodb://127.0.0.1:27017/sightings';
+mongoose.Promise = global.Promise;
+
+try {
+  connection = mongoose.connect(mongoDB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  checkServerIdentity: false,
+});
+  console.log('connection to mongodb worked!');
+} catch (e) {
+  console.log('error in db connection: ' + e.message);
+}
+
