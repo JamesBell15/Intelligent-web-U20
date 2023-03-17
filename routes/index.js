5c16312f5278ac6c83efb1a33d6324fc2926b667
JamesBell15 <73955873+JamesBell15@users.noreply.github.com>
2023-03-17 16:55:19 +0000

    Add new routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
+var multer = require('multer');
+var storage = multer.diskStorage({
+destination: function (req, file, cb) {
+    cb(null, 'public/uploads/');
+  },
+  filename: function (req, file, cb) {
+    var original = file.originalname;
+    var file_extension = original.split(".");
+    // Make the file name the date + the file extension
+    filename = Date.now() + '.' + file_extension[file_extension.length-1];
+    cb(null, filename);
+  }
+});
+var upload = multer({ storage: storage });
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const sighting_controller = require("../controllers/sighting_controller");
+
+router.get("/sighting/index", sighting_controller.index)
+router.get("/sighting/add", sighting_controller.new);
+router.post("/sighting/add", upload.single('sightingImage'), sighting_controller.create);
 
 module.exports = router;
