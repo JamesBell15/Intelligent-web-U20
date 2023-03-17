61e94508688f718b39e57bdabea1afc91b6d6aec
JamesBell15 <73955873+JamesBell15@users.noreply.github.com>
2023-03-17 16:55:11 +0000

    Update sighting model

--- models/sighting.js
+++ models/sighting.js
@@ -2,11 +2,13 @@ let mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 const sighting = new Schema({
+  active: {type: Boolean, default: true},
   userId: {type: String, required: true},
   description: {type: String, required: true, max:280},
   dateTime: {type: Date, required: true},
   identificationId: {type: String},
-  location: {type: String}
+  location: {type: String},
+  image: {type: String}
 });
 
 var Sighting = mongoose.model('Sighting', sighting);
