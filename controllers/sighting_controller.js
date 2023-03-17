ccbea53191f58b34ced98fac89b0343cfad39fd0
JamesBell15 <73955873+JamesBell15@users.noreply.github.com>
2023-03-17 16:54:54 +0000

    Add sighting contoller

---
+++ controllers/sighting_controller.js
@@ -0,0 +1,36 @@
+var bodyParser = require("body-parser");
+var multer = require('multer');
+const Sighting = require('../models/sighting')
+
+exports.new = (req, res) => {
+  res.render('sighting/add');
+};
+
+exports.create = (req, res) => {
+  let body = req.body
+  let sighting = new Sighting({
+    identificationId: body.identification,
+    userId: 'TODO',
+    location: body.location,
+    description: body.description,
+    dateTime: new Date(body.dateTime),
+    image: req.file.path
+  });
+
+  sighting.save(function (err, results) {
+      if (err) {
+        res.status(500).send('Invalid data!');
+      }else {
+        res.render('sighting/create', {sighting: sighting})
+      }
+    }
+  );
+};
+
+exports.index = (req, res) => {
+  Sighting.find().exec(function (err, sightings) {
+    if (err) err.type = 'database';
+
+    res.render('sighting/index', { sightings: sightings });
+  });
+};
