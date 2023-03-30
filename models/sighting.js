109f5bbd73d5dce0e5aad39f6f8bedd34c0fe4af
JamesBell15 <73955873+JamesBell15@users.noreply.github.com>
2023-03-30 13:01:25 +0100

    Add instance method on sighting model

--- models/sighting.js
+++ models/sighting.js
@@ -7,19 +7,13 @@ const sighting = new Schema({
   description: {type: String, required: true, max:280},
   dateTime: {type: Date, required: true},
   identificationId: {type: String},
-  location: {
-    type: {
-      type: String, // Don't do `{ location: { type: String } }`
-      enum: ['Point'], // 'location.type' must be 'Point'
-      required: true
-    },
-    coordinates: {
-      type: [Number], // Long, Lat
-      required: true
-    }
-  },
+  location: {type: String},
   image: {type: String}
 });
+
+sighting.methods.timeAsUTC = function() {
+  return this.dateTime.toUTCString();
+};
 
 var Sighting = mongoose.model('Sighting', sighting);
 module.exports = Sighting;
