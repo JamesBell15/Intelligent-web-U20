7790997873b81b3f2e99feeaa727c58250300b2e
alexlieu <alieu2001@googlemail.com>
2023-04-01 18:58:44 +0100

    update the way in which chat rooms are retrieved by loading them through the routes upon rendering the page; fix the bug discussed in previous push involving allowing a user to remain logged in; add message to be displayed when there are no rooms in the db

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,39 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+const messagesController = require('../controllers/messages')
+const {response} = require("express");
 router.get('/chat', function(req, res, next) {
-  res.render('chat');
+  messagesController.getRooms(
+      (result) => {
+        if (result.length > 0) {
+            res.render('chat', {rooms: result})
+        } else {
+          // Need to implement display message in ejs.
+          res.render('chat', {message: 'There are no existing rooms.'})
+        }
+      }
+  )
 })
+
+/**
+ *
+ * Un-used but maybe useful later.
+ *
+let username, coordinates
+let loginStatus = false
+router.post('/chat', function(req, res) {
+    username = req.body.username
+    coordinates = req.body.coords
+    loginStatus = req.body.loginStatus
+    console.log(req.body)
+    res.json({
+      username: username,
+      coords: coordinates,
+      loginStatus: loginStatus
+    })
+})
+ **/
 
 router.get('/test', function(req, res, next) {
   res.render('user');
