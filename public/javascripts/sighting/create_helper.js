cc0fcdcfca8b79d273268c217a92848939c77582
JamesBell15 <73955873+JamesBell15@users.noreply.github.com>
2023-04-04 14:35:36 +0100

    Add creating sighting offline

---
+++ public/javascripts/sighting/create_helper.js
@@ -0,0 +1,36 @@
+const addNewRecord = (requestIDB) => {
+  // create transaction and setup handlers
+  const transaction = requestIDB.result.transaction(["sightings"], "readwrite");
+
+  transaction.oncomplete = (event) => {
+    // TODO redirect to show?
+    console.log("It worked!");
+  };
+
+  transaction.onerror = (event) => {
+    console.log(`TRANS ERROR: ${event.target.errorCode}`)
+  };
+
+  let identification = document.getElementById("identification").value
+  let description = document.getElementById("description").value
+  let dateTime = document.getElementById("dateTime").value
+  let location = document.getElementById("location").value
+  let sightingImage = document.getElementById("sightingImage").value
+
+  let sighting = {
+    userId: "TODO_user",
+    description: description,
+    dateTime: new Date(dateTime),
+    identificationId: identification,
+    location: location,
+    image: sightingImage
+  }
+
+  const objectStore = transaction.objectStore("sightings");
+
+  const request = objectStore.add(sighting);
+
+  request.onsuccess = (event) => {
+    console.log(`added! ${sighting}`)
+  }
+}
