0d1d08f0d106de49c41a5a1c43af3a1589210168
JamesBell15 <73955873+JamesBell15@users.noreply.github.com>
2023-04-09 17:43:42 +0100

    Update seamless navigation between online and offline

--- public/app_sw.js
+++ public/app_sw.js
@@ -1,86 +1,126 @@
 const addResourcesToCache = async (resources) => {
-  const cache = await caches.open("v1")
-  await cache.addAll(resources)
+    const cache = await caches.open("v1")
+    await cache.addAll(resources)
 }
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    addResourcesToCache([
-      // maifest
-      "/manifest.json",
-      // css
-      "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css",
-      "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css",
-      "/stylesheets/style.css",
-      // html
-      "/html/offline.html",
-      "/partials/navbar.html",
-      "/html/sighting/new.html",
-      "/html/sighting/index.html",
-      "/html/sighting/show.html",
-      // scrtipts
-      "/app_sw.js",
-      "/jquery-3.6.4.js",
-      "/javascripts/sighting/new_helper.js",
-      "/javascripts/sighting/index_helper.js",
-      "/javascripts/sighting/show_helper.js",
-      "/javascripts/indexedDB.js"
-    ])
-  )
+    event.waitUntil(
+        addResourcesToCache([
+        // maifest
+        "/manifest.json",
+        // css
+        "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css",
+        "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css",
+        "/stylesheets/style.css",
+        // html
+        "/html/offline.html",
+        "/partials/navbar.html",
+        "/sighting/new.html",
+        "/sighting/index.html",
+        "/sighting/show.html",
+        // scrtipts
+        "/app_sw.js",
+        "/jquery-3.6.4.js",
+        "/javascripts/sighting/new_helper.js",
+        "/javascripts/sighting/index_helper.js",
+        "/javascripts/sighting/show_helper.js",
+        "/javascripts/indexedDB.js"
+        ])
+    )
 })
 
 const putInCache = async (request, response) => {
-  const cache = await caches.open("v1")
-  await cache.put(request, response)
+    const cache = await caches.open("v1")
+    await cache.put(request, response)
 }
 
 // PWA Architecture cache first
 const cacheFirst = async (request, fallbackUrl) => {
-  const responseFromCache = await caches.match(request)
+    const responseFromCache = await caches.match(request)
 
-  if (responseFromCache) {
-    return responseFromCache
-  }
-  try {
-    const responseFromNetwork = await fetch(request)
-
-    return responseFromNetwork
-  } catch (error) {
-    const fallbackResponse = await caches.match(fallbackUrl)
-    if (fallbackResponse) {
-      return fallbackResponse
+    if (responseFromCache) {
+        return responseFromCache
     }
-    // when even the fallback response is not available,
-    // there is nothing we can do, but we must always
-    // return a Response object
-    return new Response("Network error happened", {
-      status: 408,
-      headers: { "Content-Type": "text/plain" },
-    })
-  }
+    try {
+        const responseFromNetwork = await fetch(request)
+
+        return responseFromNetwork
+    } catch (error) {
+        const fallbackResponse = await caches.match(fallbackUrl)
+
+        if (fallbackResponse) {
+            return fallbackResponse
+        }
+        // when even the fallback response is not available,
+        // there is nothing we can do, but we must always
+        // return a Response object
+        return new Response(
+            "Network error happened",
+            {
+                status: 408,
+                headers: { "Content-Type": "text/plain"
+            },
+            }
+        )
+    }
 }
 
+const networkFirst = async (pathname, fallbackUrl) => {
+    try {
+        const responseFromNetwork = await fetch(pathname)
 
+        return responseFromNetwork
+    } catch (error) {
+        const responseFromCache = await caches.match(pathname + ".html")
+
+        if (responseFromCache) {
+            return responseFromCache
+        } else {
+            const fallbackResponse = await caches.match(fallbackUrl)
+            if (fallbackResponse) {
+                return fallbackResponse
+            }
+            // when even the fallback response is not available,
+            // there is nothing we can do, but we must always
+            // return a Response object
+            return new Response(
+                "Network error happened",
+                {
+                status: 408,
+                headers: { "Content-Type": "text/plain" },
+                }
+            )
+        }
+    }
+}
 
 self.addEventListener('sync', async event => {
-  if (event.tag === 'sighting-data-sync') {
-    event.waitUntil(fetch('/sighting/refresh').then(
-      (response) => response.json()).then((data) => {
-        // use this to store data from db
-        console.log(data)
-      })
-    )
-  }
+    if (event.tag === 'sighting-data-sync') {
+        event.waitUntil(fetch('/sighting/refresh').then(
+            (response) => response.json()).then((data) => {
+                // use this to store data from db
+                console.log(data)
+            })
+        )
+    }
 })
 
 self.addEventListener("fetch", (event) => {
-  let pathname = new URL(event.request.url).pathname
+    let pathname = new URL(event.request.url).pathname
 
-  if (event.request.method == "GET"){
-    if (pathname == '/sighting/index' || pathname == '/sighting/show' || pathname == '/sighting/new'){
-      event.respondWith(cacheFirst('/html' + pathname + '.html', "/html/offline.html"))
-    } else {
-      event.respondWith(cacheFirst(event.request, "/html/offline.html"))
+    if (event.request.method == "GET"){
+        if(
+            pathname == '/sighting/index.html' ||
+            pathname == '/sighting/new.html'   ||
+            pathname == '/sighting/show.html'  ||
+            pathname == '/sighting/index'      ||
+            pathname == '/sighting/show'       ||
+            pathname == '/sighting/new'
+        ) {
+            // try to get online
+            event.respondWith(networkFirst(pathname.replace('.html',''), "/html/offline.html"))
+        } else {
+            event.respondWith(cacheFirst(event.request, "/html/offline.html"))
+        }
     }
-  }
 })
--- public/javascripts/indexedDB.js
+++ public/javascripts/indexedDB.js
@@ -72,7 +72,7 @@ const setSightingId = (recordId) => {
         }
         requestUpdate.onsuccess = (event) => {
             console.log("keyset")
-            window.location.replace("/html/sighting/show.html")
+            window.location.replace("/sighting/show.html")
         }
     }
 }
