407c7710eb0483ca7b4d6257200c523d8558c481
alexlieu <alieu2001@googlemail.com>
2023-04-09 14:27:43 +0100

    fix conflicts

--- controllers/sighting_controller.js
+++ controllers/sighting_controller.js
@@ -6,7 +6,75 @@ const User = require('../models/user')
 const Message = require('../models/messages')
 
 exports.new = (req, res) => {
-    res.render('sighting/new')
+  res.render('sighting/new');
+};
+
+exports.create = async (req, res) => {
+  let body = req.body
+  const user = await User.findUser(body.user)
+  const temp = body.location.split(','),
+      longitude = Number(temp[0]), latitude = Number(temp[1])
+  console.log(typeof longitude + " " + longitude)
+  let sighting = new Sighting({
+    identificationId: body.identification,
+    userId: user,
+    location: {
+      type: 'Point',
+      coordinates: [longitude, latitude]
+    }, // -> this needs to be updated to be a numbered coordinate
+    description: body.description,
+    dateTime: new Date(body.dateTime),
+    image: Helper.extractFilePathOrURL(req)
+  });
+
+  sighting.save(async function (err, results) {
+        if (err) {
+          res.status(500).send('Invalid data!');
+        } else {
+          const findSightingByIdentification = async (identificationId, userId) => {
+            try {
+              return await Sighting.findOne({identificationId: identificationId, userId: userId}).populate('userId').exec()
+            } catch (e) {
+              console.error(e)
+            }
+          }
+          const sightingDb = await findSightingByIdentification(sighting.identificationId, sighting.userId)
+          res.redirect(`/sighting/show/${sightingDb._id}`)
+        }
+      }
+  );
+};
+
+exports.index = async (req, res) => {
+  Sighting.find().populate('userId').exec(function (err, sightings) {
+    if (err) err.type = 'database';
+    res.render('sighting/index', {sightings: sightings});
+  });
+
+  /**
+   //1 Paris - 2.3291015625000004, 48.864714761802794
+   //2 NY - -73.97094726562501, 40.697299008636755
+   //3 London - -0.087890625, 51.45400691005982
+   //4 Sheffield - -1.4282226562500002, 53.31774904749089
+   //5 Tokyo - 139.74609375000003, 35.67068501330238
+   **/
+};
+
+exports.show = (req, res) => {
+  sighting_id = req.params.id
+  Sighting.findById(sighting_id).populate('userId').exec(async function (err, sighting) {
+    if (err) {
+      err.type = 'database'
+    } else {
+      const messages = await Message.findMessagesForSighting(sighting)
+      console.log(messages)
+      res.render('sighting/show', {
+        sighting: sighting, messages: messages
+      })
+    }
+
+  });
+>>>>>>> 7a9e4a5 (implement sighting sort by distance and most recent without have to render a new page -> an issue with this is that when you enter a sighting and return to the index, the sort will not be maintained and the timeAsUTC method doesn't work because of the way sort by distance is calculated)
 }
 
 exports.create = async (req, res) => {
---
+++ routes/index.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var multer = require('multer');
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/uploads/');
+    }, filename: function (req, file, cb) {
+        var original = file.originalname;
+        var file_extension = original.split(".");
+        // Make the file name the date + the file extension
+        filename = Date.now() + '.' + file_extension[file_extension.length - 1];
+        cb(null, filename);
+    }
+});
+var upload = multer({storage: storage});
+var router = express.Router();
+
+
+const sighting_controller = require("../controllers/sighting_controller");
+
+router.get("/sighting/index", sighting_controller.index)
+router.get("/sighting/show/:id", sighting_controller.show)
+router.get("/sighting/new", sighting_controller.new)
+router.post("/sighting/new", upload.single('sightingImage'), sighting_controller.create);
+
+router.post('/api/data/sort', async function (req,res) {
+    const queries = req.body
+    if (queries.sort == 'distance') {
+        const longitude = queries.coords[0]
+        const latitude = queries.coords[1]
+        const sightings = await Sighting.sortByDistance([longitude, latitude])
+        res.render('sighting/index', {sightings: sightings})
+    }
+    if (queries.sort == 'recent') {
+        const sightings = await Sighting.sortByRecent()
+        res.render('sighting/index', {sightings: sightings})
+    }
+})
+
+const User = require('../models/user')
+const Sighting = require("../models/sighting");
+router.post('/api/data/users', async function (req, res) {
+    const userToQuery = await User.findUser(req.body.name)
+    res.json(userToQuery)
+})
+
+
+module.exports = router;
