bc7feaafe9555ca0938cf4a8b622e9c1c681b134
alexlieu <alieu2001@googlemail.com>
2023-04-10 20:41:51 +0100

    implement live search; add a clear filter button; comment out pagination as I'm unsure if it is a necessary feature

--- public/javascripts/temp.js
+++ public/javascripts/temp.js
@@ -1,22 +1,30 @@
+let sortParameters = ''
+const search = document.querySelector('#sightingSearch')
+const sortByDistance = document.querySelector('#distSort')
+const sortByRecent = document.querySelector('#timeSort')
+const sortByAlphabet = document.querySelector('#alphabetSort')
+const removeFilters = document.querySelector('#removeFilters')
+
 function getSorted(sortType) {
     try {
         useUserInfo(async (user) => {
-            let response
             switch (sortType) {
                 case "distance":
                     const coordinates = user.coords
-                    response = await fetch(`/sighting/index?sort=distance&long=${coordinates[0]}&lat=${coordinates[1]}`)
+                    sortParameters = `sort=distance&long=${coordinates[0]}&lat=${coordinates[1]}`
                     break
                 case "recent":
-                    response = await fetch(`/sighting/index?sort=recent`)
+                    sortParameters = 'sort=recent'
                     break
                 case "alphabetical":
-                    response = await fetch(`/sighting/index?sort=alphabetical`)
+                    sortParameters = 'sort=alphabetical'
                     break
                 default:
-                    response = await fetch(`/sighting/index`)
+                    sortByDistance.checked = sortByRecent.checked = sortByAlphabet. checked = false
+                    sortParameters = ''
                     break
             }
+            let response = await fetch(`/sighting/index?${sortParameters}`)
             const text = await response.text()
             updateSightingHtml(text)
         })
@@ -26,19 +34,19 @@ function getSorted(sortType) {
 }
 
 const updateSightingHtml = (text) => {
+
     const sightingList = document.querySelector('#sightingList')
-
     const div = document.createElement('div')
-    div.innerHTML = text
 
+    div.innerHTML = text
     const fragment = document.createDocumentFragment()
     const targetElems = div.querySelectorAll('#sightingBody')
-    targetElems.forEach(element => fragment.appendChild(element))
 
+    targetElems.forEach(element => fragment.appendChild(element))
     sightingList.innerHTML = ''
     sightingList.appendChild(fragment)
-}
 
+}
 const useUserInfo = (onSuccess) => {
     const db = requestIDB.result
     const store = db.transaction('userInfo', 'readonly').objectStore('userInfo')
@@ -54,20 +62,29 @@ const useUserInfo = (onSuccess) => {
     storeRequest.onerror = (event) => {
         console.error(event.target.error)
     }
+
 }
 
 const requestIDB = indexedDB.open('db', 4)
-
 requestIDB.onupgradeneeded = (event) => {
     const db = requestIDB.result
     db.createObjectStore('userInfo', {autoIncrement: false})
     console.log('IDB: Object store created.')
 }
 requestIDB.onsuccess = (event) => {
-    document.querySelector('#distSort').addEventListener('click', () => getSorted('distance'))
-    document.querySelector('#timeSort').addEventListener('click', () => getSorted('recent'))
-    document.querySelector('#alphabetSort').addEventListener('click', () => getSorted('alphabetical'))
+    sortByDistance.addEventListener('click', () => getSorted('distance'))
+    sortByRecent.addEventListener('click', () => getSorted('recent'))
+    sortByAlphabet.addEventListener('click', () => getSorted('alphabetical'))
+    removeFilters.addEventListener('click', () => getSorted())
 }
 requestIDB.onerror = (event) => {
     console.error('IDB: ' + requestIDB.error)
+
 }
+
+search.addEventListener('keyup', async (e) => {
+    let searchQuery = e.target.value.trim()
+    let response = await fetch(`/sighting/index?${sortParameters}&name=${searchQuery}`)
+    let text = await response.text()
+    updateSightingHtml(text)
+})
