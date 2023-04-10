2f14bfae654378058c6bdda88b939cb239511d02
JamesBell15 <73955873+JamesBell15@users.noreply.github.com>
2023-04-10 16:43:05 +0100

    Update displaying all records offline

--- public/javascripts/sighting/index_helper.js
+++ public/javascripts/sighting/index_helper.js
@@ -1,47 +1,48 @@
-const getAllRecords = (requestIDB) => {
+const showAllRecords = (requestIDB) => {
 
-  const objectStore = requestIDB.result.transaction("sightings").objectStore("sightings");
+    const objectStore = requestIDB.result.transaction("sightings").objectStore("sightings")
 
-  objectStore.getAll().onsuccess = (event) => {
-    showAllRecords(event.target.result)
-  };
+    request = objectStore.openCursor()
+
+    request.onerror = function(event) {
+        console.err("error fetching data")
+    }
+
+    let table = document.getElementById("sightingsDiv")
+
+    request.onsuccess = function(event) {
+        let cursor = event.target.result
+
+        if (cursor) {
+            let key = cursor.primaryKey
+            let value = cursor.value
+
+            table.innerHTML += `
+            <section sighting-post-id="${key}" id="sightingBody" class="container p-2">
+                <div class="row row-cols-5 border-bottom border-light-dark">
+                <h4 class="col fs-6"> ${value.identificationId} </h4>
+                <div class="col fs-6"> ${value.userId} </div>
+                <div class="col fs-6"> ${value.location} </div>
+                <div class="col fs-6"> ${value.dateTime} </div>
+                <div class="col fs-6"><button class="showButton" id="btn-${key}" type="button"><i class="fa-solid fa-binoculars"></i></button></div>
+                </div>
+            </section>`
+
+            let btn = document.getElementById(`btn-${key}`)
+
+            btn.addEventListener("click", (event) => {setSightingId(key)})
+
+            cursor.continue()
+        }
+        else {
+            console.log("done displaying records")
+        }
+    }
 }
 
-const showAllRecords = (records) => {
-  let table = document.getElementById("sightingsDiv")
 
-  // Set up headers
-  let output = ``
-  // remove old data
-  table.innerHTML = output
-
-  // Write each record to a row
-  for (const [key, _] in records) {
-    let record = records[key]
-
-    // Key is relative to array hence out of phase with IDB
-    let indexKey = parseInt(key) + 1
-
-    table.innerHTML += `
-    <section sighting-post-id="${indexKey}" id="sightingBody" class="container p-2">
-    <div class="row row-cols-5 border-bottom border-light-dark">
-        <h4 class="col fs-6"> ${record.identificationId} </h4>
-        <div class="col fs-6"> ${record.userId} </div>
-        <div class="col fs-6"> ${record.location} </div>
-        <div class="col fs-6"> ${record.dateTime} </div>
-        <div class="col fs-6"><button class="showButton" id="${indexKey}" type="button"><i class="fa-solid fa-binoculars"></i></button></div>
-    </div>
-    </section>`
-  }
-
-  let btns = document.getElementsByClassName('showButton')
-
-  for (let btn of btns) {
-    btn.addEventListener("click", (event) => {setSightingId(btn.id)})
-  }
-}
 
 // when indexedDB opens populate page
 requestIDB.onsuccess = (event) => {
-  getAllRecords(requestIDB)
+    showAllRecords(requestIDB)
 }
