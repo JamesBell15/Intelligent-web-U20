56cca7fda30ad30cb7a2cab075982074cbae0ace
JamesBell15 <73955873+JamesBell15@users.noreply.github.com>
2023-04-10 19:23:25 +0100

    Update routes

--- routes/sighting.js
+++ routes/sighting.js
@@ -17,20 +17,23 @@ var router = express.Router();
 
 const sighting_controller = require("../controllers/sighting_controller");
 
+// Routes accociated with online sightings
 router.get("/sighting/index", sighting_controller.index)
 router.get("/sighting/show", sighting_controller.index)
 router.get("/sighting/show/:id", sighting_controller.show)
 router.get("/sighting/new", sighting_controller.new)
 router.post("/sighting/new", upload.single('sightingImage'), sighting_controller.create);
 
+// Routes accociated with online chat messages
 const User = require('../models/user')
 router.post('/api/data/users', async function (req, res) {
     const userToQuery = await User.findUser(req.body.name)
     res.json(userToQuery)
 })
 
-
-router.get("/sighting/refresh", sighting_controller.refresh)
+// Routes accociated with syncing server and client db
+router.get("/db/get", sighting_controller.get_server_data)
+router.get("/db/update", sighting_controller.update_server_data)
 module.exports = router;
 
 
