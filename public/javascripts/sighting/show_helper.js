d59f121c7d904c405f318664b629c5d44bb6fd59
JamesBell15 <73955873+JamesBell15@users.noreply.github.com>
2023-04-11 13:06:08 +0100

    Update show to display messages

--- public/javascripts/sighting/show_helper.js
+++ public/javascripts/sighting/show_helper.js
@@ -1,18 +1,49 @@
 const showRecord = () => {
     const db = requestIDB.result
-    const currentSightingObjStr = db.transaction(["currentSighting"], "readwrite").objectStore("currentSighting")
+    const transaction = db.transaction(["currentSighting", "sightings", "messages"], "readwrite")
+
+    const currentSightingObjStr = transaction.objectStore("currentSighting")
+    const sightingObjStr = transaction.objectStore("sightings")
+    const messageObjStr = transaction.objectStore("messages")
 
     currentSightingObjStr.get('current').onsuccess = (event) => {
-        const sightingObjStr = db.transaction(["sightings"], "readwrite").objectStore("sightings")
-
         let sightingId = setSightingId(event.target.result.sightingId)
 
         console.log(sightingId)
 
         sightingObjStr.get(sightingId).onsuccess = (event) => {
-            renderHTML(event.target.result)
+            renderSightingHTML(event.target.result)
+        }
+
+
+        let cursorRequest = messageObjStr.openCursor()
+
+        cursorRequest.onsuccess = function(event) {
+            let cursor = event.target.result
+
+            if (cursor) {
+                let key = cursor.primaryKey
+                let value = cursor.value
+
+                if (sightingId == value.postId){
+                    renderChatHTML(value)
+                }
+
+                cursor.continue()
+            }
+            else {
+                let btns = document.getElementsByClassName('showButton')
+
+                for (let btn of btns){
+                    btn.addEventListener("click", (event) => {setCurrentSighting(btn.id)})
+                }
+
+                console.log("done displaying records")
+            }
         }
     }
+
+
 }
 
 const setSightingId = (sightingId) => {
@@ -23,33 +54,32 @@ const setSightingId = (sightingId) => {
     return sightingId
 }
 
-const renderHTML = (record) => {
-  let table = document.getElementById("sightingTable")
+const renderChatHTML = (record) => {
+    let table = document.getElementById("chatHistory")
 
-  // Set up headers
-  let output = `<tr>
-    <th>User</th>
-    <th>Description</th>
-    <th>Time</th>
-    <th>Identification</th>
-    <th>Location</th>
-    <th>Image</th>
-    <th></th>
-  </tr>`
+    const div = document.createElement('div')
+    div.innerHTML = `<p><strong>${record.sender}:</strong>${record.msg}</p>`
 
-  output += `
-    <tr>
-      <td>${record.userId}</td>
-      <td>${record.description}</td>
-      <td>${record.dateTime}</td>
-      <td>${record.identificationId}</td>
-      <td>${record.location}</td>
-      <td>${record.image}</td>
-    </tr>`
+    table.appendChild(div);
+}
 
-  table.innerHTML = output
+const renderSightingHTML = (record) => {
+    let table = document.getElementById("sightingTable")
+
+    // Set up headers
+    let output = `
+    <div>
+        <span>${record.userId}</span>
+        <span>${record.description}</span>
+        <span>${record.dateTime}</span>
+        <span>${record.identificationId}</span>
+        <span>${record.location}</span>
+        <span>${record.image}</span>
+    </div>`
+
+    table.innerHTML = output
 }
 
 requestIDB.onsuccess = async (event) => {
-  showRecord()
+    showRecord()
 }
