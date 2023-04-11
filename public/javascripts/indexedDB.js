d47f550632c87313e7a76d9c753cbba53927b96f
JamesBell15 <73955873+JamesBell15@users.noreply.github.com>
2023-04-11 13:04:44 +0100

    Refactor indexedDB styling and moving methods

--- public/javascripts/indexedDB.js
+++ public/javascripts/indexedDB.js
@@ -8,20 +8,20 @@ const upgradeStores = (event) => {
     const sightingStore = db.createObjectStore("sightings", { autoIncrement: true })
 
     // SCHEMA
-    sightingStore.createIndex("userId", "userId", { unique: false });
-    sightingStore.createIndex("description", "description", { unique: false });
-    sightingStore.createIndex("dateTime", "dateTime", { unique: false });
-    sightingStore.createIndex("identificationId", "identificationId", { unique: false });
-    sightingStore.createIndex("location", "location", { unique: false });
-    sightingStore.createIndex("image", "image", { unique: false });
+    sightingStore.createIndex("userId", "userId", { unique: false })
+    sightingStore.createIndex("description", "description", { unique: false })
+    sightingStore.createIndex("dateTime", "dateTime", { unique: false })
+    sightingStore.createIndex("identificationId", "identificationId", { unique: false })
+    sightingStore.createIndex("location", "location", { unique: false })
+    sightingStore.createIndex("image", "image", { unique: false })
 
     const currentSightingStore = db.createObjectStore("currentSighting", { keyPath: "key" })
 
     // SCHEMA
-    currentSightingStore.createIndex("sightingId", "sightingId", { unique: false });
+    currentSightingStore.createIndex("sightingId", "sightingId", { unique: false })
 
     // Add record to manipulate
-    const request = currentSightingStore.add({key: 'current', sightingId: null});
+    const request = currentSightingStore.add({key: 'current', sightingId: null})
     request.onsuccess = (event) => {
     console.log(`added currentSighting`)
     }
@@ -29,10 +29,10 @@ const upgradeStores = (event) => {
     const messagesStore = db.createObjectStore("messages", { autoIncrement: true })
 
     // SCHEMA
-    messagesStore.createIndex("senderId", "senderId", { unique: false });
-    messagesStore.createIndex("postId", "postId", { unique: false });
-    messagesStore.createIndex("msg", "msg", { unique: false });
-    messagesStore.createIndex("createdAt", "createdAt", { unique: false });
+    messagesStore.createIndex("senderId", "senderId", { unique: false })
+    messagesStore.createIndex("postId", "postId", { unique: false })
+    messagesStore.createIndex("msg", "msg", { unique: false })
+    messagesStore.createIndex("createdAt", "createdAt", { unique: false })
 
     console.log("Upgraded!")
 }
@@ -59,14 +59,11 @@ const setCurrentSighting = (recordId) => {
     .objectStore("currentSighting")
 
     objectStore.get('current').onsuccess = (event) => {
-        // Get the old value that we want to update
         const data = event.target.result
 
-        // update the value(s) in the object that you want to change
         data.sightingId = recordId
 
-        // Put this updated object back into the database.
-        const requestUpdate = objectStore.put(data);
+        const requestUpdate = objectStore.put(data)
         requestUpdate.onerror = (event) => {
             console.log("failed")
         }
@@ -74,16 +71,5 @@ const setCurrentSighting = (recordId) => {
             console.log("keyset")
             window.location.replace("/sighting/show")
         }
-    }
-}
-
-// fetch up todate records and store them in indexedDB
-const updateIDB = async () => {
-    const registration = await navigator.serviceWorker.ready;
-    try {
-        await registration.sync.register("sighting-data-sync");
-        console.log("Background Sync registered!");
-    } catch {
-        console.log("Background Sync could not be registered!");
     }
 }
