2e5498850ffea7050c34424b27eaa6aa5d5dde0e
JamesBell15 <73955873+JamesBell15@users.noreply.github.com>
2023-04-14 12:55:45 +0100

    Update helper to extract correct url/file

--- helpers/controller_helpers/sighting.js
+++ helpers/controller_helpers/sighting.js
@@ -1,20 +1,23 @@
 
-exports.extractFilePathOrURL = (req) => {
-  try {
-    let url = new URL(req.body.sightingImage);
-    return url
-  } catch (_) {
-    return req.file.path;
-  }
-  return req.file.path;
+exports.getImageFormReq = async (req) => {
+    let data = null, contentType = null, url = null
+
+    try {
+        url = new URL(req.body.sightingImage)
+    } catch (_) {
+        data = req.file.buffer
+        contentType = req.file.mimeType
+    }
+
+    return { data: data, contentType: contentType, url: url }
 }
 
 exports.extractFilePathOrURLFromJSON = (body) => {
-  try {
-    let url = new URL(body.image);
-    return url
-  } catch (_) {
-    return body.image;
-  }
-  return body.image;
+    try {
+        let url = new URL(body.image)
+        return url
+    } catch (_) {
+        return body.image
+    }
+    return body.image
 }
