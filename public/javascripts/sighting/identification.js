7710f57bc3932c3f90e97f6e2cd0329b4486e543
JFijalkowski <jonathanfijallkowski@gmail.com>
2023-04-17 01:48:21 +0100

    add functions to get identification details
    functions contain logic for each different input  configuration (offline/no identification options, unknown, unconfirmed and confirmed)

--- public/javascripts/sighting/identification.js
+++ public/javascripts/sighting/identification.js
@@ -1,3 +1,4 @@
+//get selection of bird species from DBPedia similar to search term
 const searchIdentifications = () =>{
     let birdName = document.getElementById("identificationSearch").value;
 
@@ -52,3 +53,56 @@ const searchIdentifications = () =>{
 
 let searchButton = document.getElementById("findIdentifications");
 searchButton.addEventListener("click", searchIdentifications);
+
+//get confirmation status of sighting
+//returns "unknown", "unconfirmed" and "confirmed" (lowercase)
+function getConfirmation() {
+    //if user has checked the "unknown" checkbox
+    if(document.getElementById("unknownIdentification").value){
+        return "unknown";
+    }
+    //if no options are present, but user has not checked the "unknown" checkbox (user is offline)
+    else if(!optionsPopulated()){
+        return "unconfirmed";
+    }
+    //if user has selected the "identification confirmed" checkbox
+    else if(document.getElementById("confirmIdentification").value){
+        return "confirmed"
+    }
+    //otherwise, identification is unconfirmed
+    else{
+        return "unconfirmed"
+    }
+}
+
+//check if identification options section is populated
+//returns true if there are options in the dropdown
+function optionsPopulated(){
+    if (document.getElementById("identification").options.length < 1){return false}
+    else{return true}
+}
+
+//get identifications (DBPedia URI and label) from frontend
+//handles different input selections
+function getIdentificationDetails(){
+    let details = [null,""];
+    //if user has selected "unknown", ignore other inputs
+    if (getConfirmation() === "unknown"){
+        return details;
+    }
+    //if no options are provided (user offline or naming is incorrect)
+    else if (!optionsPopulated()){
+        //no options provided, so don't try to get value from dropdown
+        //use entered search term instead in lieu of a verified identification
+        details[1] = document.getElementById("identificationSearch").value;
+    }
+    //otherwise, there should be options to select from
+    else{
+        let identification = document.getElementById("identification");
+        //get identification DBPedia URI (value of dropdown selection)
+        details[0] = identification.value;
+        //get name of bird (label attribute of DBPedia resource)
+        details[1] = identification.options[identification.selectedIndex].text;
+    }
+    return details;
+}
