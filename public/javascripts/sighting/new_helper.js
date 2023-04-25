52ebc84be06518d0cd807e105ebb76fb04795dae
JamesBell15 <73955873+JamesBell15@users.noreply.github.com>
2023-04-25 17:50:04 +0100

    Update new helper to use user input

--- public/javascripts/sighting/new_helper.js
+++ public/javascripts/sighting/new_helper.js
@@ -1,6 +1,4 @@
 const addNewRecord = (requestIDB) => {
-
-
     //no identification URI can be provided, set to default of "unknown"
     let identificationURI = "unknown";
     //offline sighting = unconfirmed sighting (since knowledge graph cannot be reached)
@@ -23,6 +21,8 @@ const addNewRecord = (requestIDB) => {
 
     let description = document.getElementById("description").value
     let dateTime = document.getElementById("dateTime").value
+    let username = document.getElementById("user").value
+
 
     const temp = document.getElementById("location").value.split(','),
         longitude = Number(temp[0]), latitude = Number(temp[1])
@@ -46,52 +46,41 @@ const addNewRecord = (requestIDB) => {
 
             let sightingImage = { data: btoa(data), contentType: contentType, url: url  }
 
-            insertIntoIDB(description, dateTime, identificationURI, identificationName, confirmation, location, sightingImage)
+            insertIntoIDB(username, description, dateTime, identificationURI, identificationName, confirmation, location, sightingImage)
         }
     } else if (url != '') {
         let sightingImage = { data: null, contentType: null, url: url  }
 
-        insertIntoIDB(description, dateTime, identificationURI, identificationName, confirmation, location, sightingImage)
+        insertIntoIDB(username, description, dateTime, identificationURI, identificationName, confirmation, location, sightingImage)
     }
 }
 
-const insertIntoIDB = async (description, dateTime, identificationURI, identificationName, confirmation, location, image) => {
-    const transaction = requestIDB.result.transaction(["sightings", 'userInfo'], "readwrite")
+const insertIntoIDB = async (username, description, dateTime, identificationURI, identificationName, confirmation, location, image) => {
+    const transaction = requestIDB.result.transaction(["sightings"], "readwrite")
 
     transaction.onerror = (event) => {
         console.log(`TRANS ERROR: ${event.target.errorCode}`)
     }
 
-    const userStore = transaction.objectStore("userInfo")
-    let newSightingId
+    let sighting = {
+        userId: username,
+        description: description,
+        dateTime: new Date(dateTime),
+        identificationURI: identificationURI,
+        identificationName: identificationName,
+        confirmation: confirmation,
+        location: location,
+        image: image
+    }
 
-    userStore.get('user').onsuccess = async (event) => {
-        const user = event.target.result
+    const sightingStore = transaction.objectStore("sightings")
 
-        if (user) {
-            let sighting = {
-                userId: user.username,
-                description: description,
-                dateTime: new Date(dateTime),
-                identificationURI: identificationURI,
-                identificationName: identificationName,
-                confirmation: confirmation,
-                location: location,
-                image: image
-            }
+    const request = await sightingStore.add(sighting)
 
-            const sightingStore = transaction.objectStore("sightings")
+    request.onsuccess = async (event) => {
+        newSightingId = await event.target.result
 
-            const request = await sightingStore.add(sighting)
-
-            request.onsuccess = async (event) => {
-                newSightingId = await event.target.result
-
-                registerNewSightingSync(newSightingId)
-            }
-        } else {
-            console.log('user not logged in.')
-        }
+        registerNewSightingSync(newSightingId)
     }
 
     transaction.oncomplete = () => {
