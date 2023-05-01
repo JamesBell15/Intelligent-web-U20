31a31df41fe8fd751d0dfd943083d42f62429f84
JamesBell15 <73955873+JamesBell15@users.noreply.github.com>
2023-05-01 12:38:56 +0100

    Fix sw not caching header_helper.js

--- public/app_sw.js
+++ public/app_sw.js
@@ -3,28 +3,39 @@ self.importScripts("/javascripts/serviceWorkerHelper.mjs")
 self.addEventListener("install", (event) => {
     event.waitUntil(
         addResourcesToCache([
-        // manifest
-        "/manifest.json",
-        // css
-        "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css",
-        "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css",
-        "/stylesheets/style.css",
-        // html
-        "/html/offline.html",
-        "/partials/navbar.html",
-        "/sighting/new.html",
-        "/sighting/index.html",
-        "/sighting/show.html",
-        // scripts
-        "/app_sw.js",
-        "/jquery-3.6.4.js",
-        "/javascripts/sighting/new_helper.js",
-        "/javascripts/sighting/index_helper.js",
-        "/javascripts/sighting/show_helper.js",
-        "/javascripts/sighting/identification.js",
-        "/javascripts/indexedDB.js",
-        "/javascripts/header_helper.js",
-        "/javascripts/form.js"
+            // manifest
+            "/manifest.json",
+            // css
+            "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css",
+            "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css",
+            "/stylesheets/style.css",
+            // html
+            "/html/offline.html",
+            "/partials/navbar.html",
+            "/sighting/new.html",
+            "/sighting/index.html",
+            "/sighting/show.html",
+            // scripts
+            "/app_sw.js",
+            "/jquery-3.6.4.js",
+            "/javascripts/subscription_helper.mjs",
+            "/javascripts/sighting/new_helper.js",
+            "/javascripts/sighting/index_helper.js",
+            "/javascripts/sighting/show_helper.js",
+            "/javascripts/sighting/identification.js",
+            "/javascripts/indexedDB.js",
+            "/javascripts/header_helper.js",
+            "/javascripts/form.js",
+            // icons
+            "/icons/icon-48x48.png",
+            "/icons/icon-72x72.png",
+            "/icons/icon-96x96.png",
+            "/icons/icon-128x128.png",
+            "/icons/icon-144x144.png",
+            "/icons/icon-152x152.png",
+            "/icons/icon-192x192.png",
+            "/icons/icon-384x384.png",
+            "/icons/icon-512x512.png"
         ])
     )
 })
