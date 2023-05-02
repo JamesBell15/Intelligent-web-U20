f5056ad395a52fab71aa9938c8848fc0129eca62
JFijalkowski <jonathanfijallkowski@gmail.com>
2023-05-02 02:45:51 +0100

    get show page to pull data from DBPedia
    styling definitely needs some work, it's a very botched job, but it works

--- public/javascripts/sighting/show_dbpedia.js
+++ public/javascripts/sighting/show_dbpedia.js
@@ -1,60 +1,82 @@
 //get details of bird from related DBPedia page
-const getIdentificationDetails = () =>{
-    let uri = document.getElementById("DBPediaLink").innerText
 
-    // The DBpedia SPARQL endpoint URL being queried
-    const endpointUrl = "https://dbpedia.org/sparql"
+let uri = document.getElementById("DBPediaLink").innerText
 
-    // SPARQL query to return a list of birds with names similar to the entered query
-    // TODO: get names that are "similar" / "LIKE" the query in case of misspellings
-    // TODO: get some way of prioritising "more relevant" entities
-    const sparqlQuery = `
-        PREFIX dbo: <http://dbpedia.org/ontology/>
-        PREFIX dbprop: <http://dbpedia.org/property/>
-        PREFIX dbr: <http://dbpedia.org/resource>
-        
-        select distinct ?label ?abstract ?species ?genus ?thumbnail
-        where {
-            `+uri+` rdf:type dbo:Bird;
-                rdfs:label ?label;
-                dbo:abstract ?abstract.
-            OPTIONAL{
-            `+uri+` dbp:genus ?genus;
-                dbp:species ?species;
-                dbo:thumbnail ?thumbnail.}
-            FILTER(langMatches(lang(?label), "en" ))
-            FILTER(langMatches(lang(?abstract), "en" ))
-        } LIMIT 15`
+// The DBpedia SPARQL endpoint URL being queried
+const endpointUrl = "https://dbpedia.org/sparql"
 
-    // Encode the query as a URL parameter
-    const encodedQuery = encodeURIComponent(sparqlQuery)
-    // Build the URL for the SPARQL query
-    const url = `${endpointUrl}?query=${encodedQuery}&format=json`
+// SPARQL query to return a list of birds with names similar to the entered query
+// TODO: get names that are "similar" / "LIKE" the query in case of misspellings
+// TODO: get some way of prioritising "more relevant" entities
+const sparqlQuery = `
+    PREFIX dbo: <http://dbpedia.org/ontology/>
+    PREFIX dbprop: <http://dbpedia.org/property/>
+    PREFIX dbr: <http://dbpedia.org/resource/>
+    
+    select distinct ?label ?abstract ?species ?genus ?thumbnail
+    where {
+        <`+uri+`> rdf:type dbo:Bird;
+            rdfs:label ?label;
+            dbo:abstract ?abstract.
+        OPTIONAL{
+        <`+uri+`> dbp:genus ?genus;
+            dbp:species ?species;
+            dbo:thumbnail ?thumbnail.}
+        FILTER(langMatches(lang(?label), "en" ))
+        FILTER(langMatches(lang(?abstract), "en" ))
+    } LIMIT 15`
 
-    // Use fetch to retrieve the data
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            // The results are in the 'data' object
-            let bindings = data.results.bindings
-            let result = JSON.stringify(bindings)
-            //add each returned value to dropdown
+// Encode the query as a URL parameter
+const encodedQuery = encodeURIComponent(sparqlQuery)
+// Build the URL for the SPARQL query
+const url = `${endpointUrl}?query=${encodedQuery}&format=json`
+
+// Use fetch to retrieve the data
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+    // The results are in the 'data' object
+    let bindings = data.results.bindings
+    let result = JSON.stringify(bindings)
+    console.log(bindings[0])
+    //create and populate div with the info received from DBPedia
+    let dataBox = document.getElementById("DBPediaLink")
+    let bird = bindings[0];
+
+    //get and display name of bird from DBPedia entry
+    let name = document.createElement("h2")
+    name.id = "DBPName"
+    name.innerHTML = bird.label.value
+    dataBox.appendChild(name);
+
+    //get latin binomial nomenclature (append genus and species)
+    let latinName = ""
+    //values may not have been present in dbpedia entry
+    if(bird.genus.value){
+        latinName += bird.genus.value + " "
+    }
+    if(bird.species.value){
+        latinName += bird.species.value
+    }
+    let latin = document.createElement("h3")
+    latin.id = "latinName"
+    latin.innerHTML = latinName
+    dataBox.append(latin)
+
+    //get dbpedia entry's image (if present)
+    if(bird.thumbnail.value){
+        let birdImg = document.createElement("img")
+        birdImg.src = bird.thumbnail.value
+        dataBox.append(birdImg)
+    }
+
+    //add description ("abstract" attribute of DBPedia entry, which is always present)
+    let description = document.createElement("div")
+    description.id = "DBPDescription"
+    description.innerHTML = bird.abstract.value
+    description.style.overflow = "auto"
+    description.style.maxHeight = "300px"
+    dataBox.appendChild(description)
 
 
-            let dropdown = document.getElementById("identification")
-            while (dropdown.options.length >0){dropdown.options.remove(0)}
-
-            let identificationWrapper = document.getElementById("identificationInputs")
-            for (i in bindings){
-
-                //create option per result from query
-                let opt = document.createElement("option")
-                let bird = bindings[i]
-                opt.value = bird.bird.value
-                opt.innerHTML = bird.label.value
-                dropdown.appendChild(opt)
-            }
-            //make inputs visible as they are now populated
-            identificationWrapper.style.visibility= "visible"
-        })
-}
+    }).catch(err => {return err});
