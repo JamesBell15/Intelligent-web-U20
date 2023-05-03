0ccbc0e5f5a8427507c0559e9e1efed11c411ea1
JFijalkowski <79927139+JFijalkowski@users.noreply.github.com>
2023-05-03 13:32:07 +0100

    DBPedia info on Show (#27)
    * Fix #16
    
    * (WIP) add query to get dbpedia data
    
    * get show page to pull data from DBPedia
    
    styling definitely needs some work, it's a very botched job, but it works
    
    * fix query and optional field failures
    
    * delete spaqrl query testing file
    
    * hide link when unknown identification
    
    * rework DBPedia details section structure
    
    * add image caption / alt text
    
    * finish restructure
    
    * fix unknown identification detection
    
    * remove console logs and semicolons
    
    you're welcome james
    
    * Update public/javascripts/sighting/show_dbpedia.js
    
    Co-authored-by: JamesBell15 <73955873+JamesBell15@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: JamesBell15 <73955873+JamesBell15@users.noreply.github.com>

---
+++ public/javascripts/sighting/show_dbpedia.js
@@ -0,0 +1,89 @@
+//get details of bird from related DBPedia page
+
+let uri = document.getElementById("DBPediaURI").href
+// The DBpedia SPARQL endpoint URL being queried
+const endpointUrl = "https://dbpedia.org/sparql"
+
+// SPARQL query to return a list of birds with names similar to the entered query
+// TODO: get names that are "similar" / "LIKE" the query in case of misspellings
+// TODO: get some way of prioritising "more relevant" entities
+const sparqlQuery = `
+    PREFIX dbo: <http://dbpedia.org/ontology/>
+    PREFIX dbprop: <http://dbpedia.org/property/>
+    PREFIX dbr: <http://dbpedia.org/resource/>
+    
+    select distinct ?label ?abstract ?species ?genus ?thumbnail ?caption
+    where {
+        <`+uri+`> rdf:type dbo:Bird;
+            rdfs:label ?label;
+            dbo:abstract ?abstract.
+        OPTIONAL{
+        <`+uri+`> dbp:genus ?genus.}
+        OPTIONAL{
+        <`+uri+`> dbp:species ?species.}
+        OPTIONAL{
+        <`+uri+`> dbo:thumbnail ?thumbnail.}
+        OPTIONAL{
+        <`+uri+`> dbprop:imageCaption ?caption.}
+        FILTER(langMatches(lang(?label), "en" ))
+        FILTER(langMatches(lang(?abstract), "en" ))
+        FILTER(langMatches(lang(?caption), "en" ))
+    } LIMIT 15`
+
+// Encode the query as a URL parameter
+const encodedQuery = encodeURIComponent(sparqlQuery)
+// Build the URL for the SPARQL query
+const url = `${endpointUrl}?query=${encodedQuery}&format=json`
+
+// Use fetch to retrieve the data
+if(document.getElementById("identificationURI").value === "unknown") {
+    let dataBox = document.getElementById("DBPediaData")
+    dataBox.style.display = "none"
+}
+else{
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            // The results are in the 'data' object
+            let bindings = data.results.bindings
+            let result = JSON.stringify(bindings)
+            //create and populate div with the info received from DBPedia
+            let dataBox = document.getElementById("DBPediaData")
+            let bird = bindings[0]
+
+            //get and display name of bird from DBPedia entry
+            let name = document.getElementById("DBPName")
+            name.innerHTML = bird.label.value + " "
+
+            //move link into name element now it's populated (would otherwise be overwritten)
+            let link = document.getElementById("DBPediaURI")
+            name.appendChild(link)
+
+            //get latin binomial nomenclature (append genus and species)
+            let latinName = ""
+            //values may not have been present in dbpedia entry
+            if("genus" in bird){
+                latinName += bird.genus.value + " "
+            }
+            if("species" in bird){
+                latinName += bird.species.value
+            }
+            let latin = document.getElementById("DBPLatinName")
+            latin.innerHTML = latinName
+
+            let birdImg = document.getElementById("DBPthumbnail")
+            //get dbpedia entry's image (if present)
+            if("thumbnail" in bird){
+                birdImg.src = bird.thumbnail.value
+            }
+            //add image caption as alt & hover text
+            if("caption" in bird) {
+                birdImg.title= bird.caption.value
+                birdImg.alt = "image of: " + bird.caption.value
+            }
+            //add description ("abstract" attribute of DBPedia entry, which is always present)
+            let description = document.getElementById("DBPDescription")
+            description.innerHTML = bird.abstract.value
+        }).catch(err => {console.log(err)})
+}
+
