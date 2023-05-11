9151728673e933e1bacda3937fe07115a266ff4e
JamesBell15 <73955873+JamesBell15@users.noreply.github.com>
2023-05-11 11:22:00 +0100

    Clean up styling

--- databases/database.js
+++ databases/database.js
@@ -1,15 +1,15 @@
-let mongoose = require('mongoose');
-let mongoDB = 'mongodb://127.0.0.1:27017/database';
-mongoose.Promise = global.Promise;
+let mongoose = require('mongoose')
+let mongoDB = 'mongodb://127.0.0.1:27017/database'
+mongoose.Promise = global.Promise
 
 try {
-  connection = mongoose.connect(mongoDB, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  checkServerIdentity: false,
-});
-  console.log('connection to mongodb worked!');
+    connection = mongoose.connect(mongoDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    checkServerIdentity: false,
+})
+    console.log('connection to mongodb worked!')
 } catch (e) {
-  console.log('error in db connection: ' + e.message);
+    console.log('error in db connection: ' + e.message)
 }
 
