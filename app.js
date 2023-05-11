a15e6e295dbbffe54b97cdd56e817b9c1e944450
JamesBell15 <73955873+JamesBell15@users.noreply.github.com>
2023-05-11 11:22:22 +0100

    Clean up app styling and remove unused user route

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require('http-errors')
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
 var expressLayouts = require('express-ejs-layouts')
 
-var usersRouter = require('./routes/users');
 var sightingRouter = require('./routes/sighting')
 
-var app = express();
+var app = express()
 
 // View engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
-app.use(logger('dev'));
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json({limit: '50mb'}))
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Layout support for ejs
 app.use(expressLayouts)
@@ -26,23 +25,22 @@ app.set('layout', 'layouts/main')
 // Place all script blocks at the end
 app.set('layout extractScripts', true)
 
-app.use('/', sightingRouter);
-app.use('/users', usersRouter);
+app.use('/', sightingRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
-});
+  next(createError(404))
+})
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message
+    res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+    // render the error page
+    res.status(err.status || 500)
+    res.render('error')
+})
 
-module.exports = app;
+module.exports = app
